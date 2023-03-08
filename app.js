49d25ea52e54167f4bae03ca93492e1effdfced9
SManeesha99 <it20649012@my.sliit.lk>
2023-03-08 14:30:46 +0530

    Last Commit

--- app.js
+++ app.js
@@ -20,3 +20,48 @@ res.writeHead(200, {'Content-Type': 'text/html'});
 res.write('Hello World!');
 res.end();
 }).listen(8080);
+
+//Making an HTTP request commit
+const https = require('https');
+https.get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
+let data = '';
+resp.on('data', (chunk) => {
+data += chunk;
+});
+resp.on('end', () => {
+console.log(JSON.parse(data));
+});
+}).on('error', (err) => {
+console.log("Error: " + err.message);
+});
+
+//Using a module commit
+const myModule = require('./my-module.js');
+console.log(myModule.myFunction());
+
+
+//Promises commit
+const myPromise = new Promise((resolve, reject) => {
+    if (condition) {
+    resolve('Success!');
+    } else {
+    reject('Failure!');
+    }
+    });
+    myPromise.then((result) => {
+    console.log(result);
+    }).catch((error) => {
+    console.log(error);
+    });
+
+
+//Async/Await commit
+async function myFunction() {
+    try {
+    const result = await myPromise;
+    console.log(result);
+    } catch (error) {
+    console.log(error);
+    }
+    }
+    myFunction();    
