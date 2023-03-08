d183cd6397e9d3411c3c4fd8f490a3b8fc201078
SManeesha99 <it20649012@my.sliit.lk>
2023-03-08 14:17:35 +0530

    Commit 02 - write file commit

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ fs.readFile('file.txt', 'utf8', function (err, data) {
 if (err) throw err;
 console.log(data);
 });
+
+//Write file commit
+const fs = require('fs');
+fs.writeFile('file.txt', 'Hello World!', function (err) {
+if (err) throw err;
+console.log('File saved!');
+});
